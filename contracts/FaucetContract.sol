6eff96811b45045943cd335b58c89340b9e936a8
Marcos Maverick <marquinhopimpo@gmail.com>
2022-01-21 17:52:08 -0300

    contract Testing and addFunds

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    receive() external payable {}
+
+    function addFunds() external payable{}
+
+    function justTesting() external pure  returns(uint) {
+        return 2 + 2;
+    }
+}
+
